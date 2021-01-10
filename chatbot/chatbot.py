13595aa3f301b8c5d2d6f2607bb4933b52b9990f
Aexki <50047387+Aexki@users.noreply.github.com>
2021-01-10 19:39:41 +0530

    updated again

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -15,10 +15,10 @@ from tensorflow.keras.models import load_model
 lemmatizer = WordNetLemmatizer() 
 
 path=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'chatbot')
-intents = json.loads(open(path+'intents.json').read())
-words = pickle.load(open(path+'words.pkl', 'rb'))
-classes = pickle.load(open(path+'classes.pkl', 'rb'))
-model = load_model(path+'chatbotmodel.h5')
+intents = json.loads(open(path+'/intents.json').read())
+words = pickle.load(open(path+'/words.pkl', 'rb'))
+classes = pickle.load(open(path+'/classes.pkl', 'rb'))
+model = load_model(path+'/chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
